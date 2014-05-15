d5e045dab7c5192f596514ef0a3424ed4888979a
pacogomezedutic <edutic.es@gmail.es>
2014-05-15 09:40:21 +0200

    Revert "Nuevo objeto"
    This reverts commit 7bb1c7441de15ce685794c754675bcb5cab6d1e7.

--- VentanaMundo/src/VentanaMundo.java
+++ VentanaMundo/src/VentanaMundo.java
@@ -3,7 +3,6 @@ public class VentanaMundo {
 	
 	public static void main(String[] args) {
 		SumaMundo s=new SumaMundo(10,11);
-		SumaMundo s3=new SumaMundo(6,11);
 		
 		System.out.println(s.suma());
 
