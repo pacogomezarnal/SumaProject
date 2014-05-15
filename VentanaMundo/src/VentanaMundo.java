4a21769c1118a173bf8f09a8012caeacd315b6ea
pacogomezedutic <edutic.es@gmail.es>
2014-05-15 08:52:50 +0200

    Suma de otros numeros
    Signed-off-by: pacogomezedutic <edutic.es@gmail.es>

--- VentanaMundo/src/VentanaMundo.java
+++ VentanaMundo/src/VentanaMundo.java
@@ -2,7 +2,7 @@
 public class VentanaMundo {
 	
 	public static void main(String[] args) {
-		SumaMundo s=new SumaMundo(5,6);
+		SumaMundo s=new SumaMundo(10,11);
 		
 		System.out.println(s.suma());
 
