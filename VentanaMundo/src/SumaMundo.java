eb290f50bf349656585cab0c6c593182ea254823
pacogomezarnal <fjgomez@florida-uni.es>
2014-05-15 08:45:41 +0200

    Inicio de proyecto
    Signed-off-by: pacogomezarnal <fjgomez@florida-uni.es>

---
+++ VentanaMundo/src/SumaMundo.java
@@ -0,0 +1,15 @@
+
+public class SumaMundo {
+	private int a=0;
+	private int b=0;
+	public SumaMundo(int num1,int num2) {
+		a=num1;
+		b=num2;
+	}
+	
+	public int suma()
+	{
+		return a+b;
+	}
+
+}
---
+++ VentanaMundo/src/VentanaMundo.java
@@ -0,0 +1,11 @@
+
+public class VentanaMundo {
+	
+	public static void main(String[] args) {
+		SumaMundo s=new SumaMundo(5,6);
+		
+		System.out.println(s.suma());
+
+	}
+
+}
