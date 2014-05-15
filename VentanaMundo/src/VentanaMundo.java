45793ce200f4500cfff17f21ee90b6f2b28adcef
pacogomezedutic <edutic.es@gmail.es>
2014-05-15 09:50:23 +0200

    Modificacion Signed-off-by: pacogomezedutic <edutic.es@gmail.es>

--- VentanaMundo/src/VentanaMundo.java
+++ VentanaMundo/src/VentanaMundo.java
@@ -4,6 +4,7 @@ public class VentanaMundo {
 	public static void main(String[] args) {
 		SumaMundo s=new SumaMundo(10,11);
 		SumaMundo s2=new SumaMundo(15,16);
+		SumaMundo s3=new SumaMundo(12,16);
 		
 		System.out.println(s.suma());
 		System.out.println(s2.suma());
