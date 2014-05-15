3e310025ee59852c94cbaef9a30f5aa64f19abe9
pacogomezedutic <edutic.es@gmail.es>
2014-05-15 09:07:36 +0200

    Añadido nuevo objeto
    Signed-off-by: pacogomezedutic <edutic.es@gmail.es>

--- VentanaMundo/src/VentanaMundo.java
+++ VentanaMundo/src/VentanaMundo.java
@@ -2,7 +2,7 @@
 public class VentanaMundo {
 	
 	public static void main(String[] args) {
-		SumaMundo s=new SumaMundo(5,6);
+		SumaMundo s=new SumaMundo(10,11);
 		SumaMundo s2=new SumaMundo(15,16);
 		
 		System.out.println(s.suma());
